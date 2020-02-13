66e82494d06251e5e7dee5de8d1a25e500be5082
Jorge Lopez <JorgeLopez5252@gmail.com>
2020-02-13 00:49:29 -0700

    last update before the night

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,67 +3,72 @@ import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css";
 
 export default class Projects extends Component {
-
-    componentDidMount(){
-        document.addEventListener('DOMContentLoaded', function() {
-            var elems = document.querySelectorAll('.slider');
-            var instances = M.Slider.init(elems, {});
-          });    
-    }
+  componentDidMount() {
+    document.addEventListener("DOMContentLoaded", function() {
+      var elems = document.querySelectorAll(".slider");
+      var instances = M.Slider.init(elems, {});
+    });
+  }
 
   render() {
-      return (
-
-<div className="container">
-      <div className="row">
-        <div className="col s6">Col 1</div>
-        <div className="col s6">Col 2</div>
-      </div>
+    return (
       <div className="container">
-        <div className="slider">
-          <ul className="slides">
-            <li>
-              <img src="https://lorempixel.com/580/250/nature/1" />
-              <div className="caption center-align">
-                <h3>This is our big Tagline!</h3>
-                <h5 className="light grey-text text-lighten-3">
-                  Here's our small slogan.
-                </h5>
-              </div>
-            </li>
-            <li>
-              <img src="https://lorempixel.com/580/250/nature/2" />
-              <div className="caption left-align">
-                <h3>Left Aligned Caption</h3>
-                <h5 className="light grey-text text-lighten-3">
-                  Here's our small slogan.
-                </h5>
-              </div>
-            </li>
-            <li>
-              <img src="https://lorempixel.com/580/250/nature/3" />
-              <div className="caption right-align">
-                <h3>Right Aligned Caption</h3>
-                <h5 className="light grey-text text-lighten-3">
-                  Here's our small slogan.
-                </h5>
-              </div>
-            </li>
-            <li>
-              <img src="https://lorempixel.com/580/250/nature/4" />
-              <div className="caption center-align">
-                <h3>This is our big Tagline!</h3>
-                <h5 className="light grey-text text-lighten-3">
-                  Here's our small slogan.
-                </h5>
-              </div>
-            </li>
-          </ul>
+        <div className="row">
+          <p>
+            Below are a few projects I have had the pleasure in working on with
+            both groups as well as independently.
+          </p>
+        </div>
+        <div className="container">
+          <div className="slider">
+            <ul className="slides">
+              <li>
+                <img src="https://cdn.spacetelescope.org/archives/images/wallpaper2/heic1509a.jpg" />
+                <div className="caption center-align">
+                  <h3><a target="blank" href="https://jorgelopez5252.github.io/city2city/">City2City</a></h3>
+                  <h5 className="light grey-text text-lighten-3">
+                    Group Project
+                  </h5>
+                </div>
+              </li>
+              <li>
+                <img src="https://images.pexels.com/photos/110854/pexels-photo-110854.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                <div className="caption center-align">
+                  <h3><a target="blank" href="https://obscure-tor-81991.herokuapp.com/home">Tempus</a></h3>
+                  <h5 className="light grey-text text-lighten-3">
+                    Group Project
+                  </h5>
+                </div>
+              </li>
+              <li>
+                <img src="https://inteng-storage.s3.amazonaws.com/img/iea/PrO3onAKOq/sizes/new-project-1_resize_md.jpg" />
+                <div className="caption center-align">
+                  <h3><a target="blank" href="https://morning-fortress-81693.herokuapp.com/">Note Taker</a></h3>
+                  <h5 className="light grey-text text-lighten-3">
+                    Individual project
+                  </h5>
+                </div>
+              </li>
+              <li>
+                <img src="https://cdn.technologyreview.com/i/images/iss050e066094large.jpg?sw=959&cx=0&cy=148&cw=1920&ch=1080" />
+                <div className="caption center-align">
+                  <h3>Open Garage!</h3>
+                  <h5 className="light grey-text text-lighten-3">
+                    Indivdual Project
+                  </h5>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="row">
+          <p>
+            The four projects being displayed above are fully funcitional and
+            deployed, please have a look at the them and follow the
+            corresponding links!
+          </p>
         </div>
       </div>
-    </div>
-
-      )
-    ;
+    );
   }
 }
