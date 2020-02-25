d6d746ad9a9d4124f73fd0aad0556ba5c23082e4
Jorge Lopez <JorgeLopez5252@gmail.com>
2020-02-25 14:30:01 -0700

    replaced images with tech logos and updated bio

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,27 +1,34 @@
 import React from "react";
 import Parallax from "../Parallax";
+import "./Landing.css";
 
 function Landing() {
   return (
     <div>
       <Parallax
-          paragraph="Duke it out with 4 of your friends or foes, in this turned based,tactical, and unpredictable game experience!"
-          title="Jorge Antonio Lopez"
-          url="https://scontent.fphx1-2.fna.fbcdn.net/v/t1.0-9/556280_419147001430778_2117318718_n.jpg?_nc_cat=100&_nc_ohc=QKfiUVoZ4vEAX8EVoBO&_nc_ht=scontent.fphx1-2.fna&oh=bdc5935885393403e7cd9a7569bacdeb&oe=5EF2F496"
-        />
+        paragraph="I was born in San Diego, California but moved to Mesa, Arizona when I was 7 years old. I guess you could say I am a native here at this point. I am very passionate about working with others, the collaboration of many ideas fills my passion on being a Full Stack Web Developer. I am no stranger to the weird and uncomfortable, and I feel the best way to learn is to place yourself in the unfamiliar and unknown as your own results can and will surprise you!"
+        title="Jorge Antonio Lopez"
+        url="https://scontent.fphx1-2.fna.fbcdn.net/v/t1.0-9/556280_419147001430778_2117318718_n.jpg?_nc_cat=100&_nc_ohc=QKfiUVoZ4vEAX8EVoBO&_nc_ht=scontent.fphx1-2.fna&oh=bdc5935885393403e7cd9a7569bacdeb&oe=5EF2F496"
+      />
 
-        <Parallax
-          paragraph="Each player will be allowed to make once choice on a possible play or outcome  during a LIVE GAME (Tackle, Pass, Run , Field Goal, ETC) that will be active for the current play/drive and another that will be placed in que, the latter will then come up on the next play"
-          title="Make the call!"
-          url="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/back-view-shot-of-the-beautiful-professional-gamer-royalty-free-image-1010650900-1564417674.jpg?crop=0.757xw:1.00xh;0.109xw,0&resize=640:*"
-        />
+      <Parallax
+        paragraph="Proficient Skills"
+        title="Known Technologies"
+        url="https://github.com/Jorgelopez5252/reactportfolio/blob/master/public/images/trifecta.jpg?raw=true"
+      />
 
-        <Parallax
-          paragraph="Using advance updates via API, live gameplay action will be provided via our feed and if your selection matches the play or Outcome you had decided on, then you shall be awarded a point towards your score"
-          title="Live Action, Live Updates!"
-          url="https://cdn.vox-cdn.com/thumbor/9ovEcbVj59oLaNMOxK4H02tlNGI=/0x0:4272x2524/1200x800/filters:focal(1875x700:2557x1382)/cdn.vox-cdn.com/uploads/chorus_image/image/65905530/usa_today_11987845.0.jpg"
-        />
-<div>Footer goes down here</div>
+      <Parallax
+        paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris dolor mi, aliquet ac tristique vel, molestie sed ex. Cras blandit ante urna, et aliquam augue tempus iaculis. Curabitur gravida ut magna ac mollis."
+        title="node.js"
+        url="https://github.com/Jorgelopez5252/reactportfolio/blob/master/public/images/nodeJs.jpg?raw=true"
+      />
+
+      <Parallax
+        paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris dolor mi, aliquet ac tristique vel, molestie sed ex. Cras blandit ante urna, et aliquam augue tempus iaculis. Curabitur gravida ut magna ac mollis."
+        title="React.js"
+        url="https://github.com/Jorgelopez5252/reactportfolio/blob/master/public/images/reactWhite.png?raw=true"
+      />
+      <div>Footer goes down here</div>
     </div>
   );
 }
