b9e2d5ce31f891af8307ce9724b1063e85e149bf
Jorge Lopez <JorgeLopez5252@gmail.com>
2020-03-03 17:44:27 -0700

    created basis for contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,21 +2,24 @@ import React from "react";
 
 function Contact() {
   return (
-    <div class="row">
-    <div class="col s12 m6">
-      <div class="card blue-grey darken-1">
-        <div class="card-content white-text">
-          <span class="card-title">Card Title</span>
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
+    <div class="container">
+      <div class="row">
+        <div class="col s6">
+          <h2>About me</h2>
+          <p>I was born in San Diego, California but moved to Mesa, Arizona when I was 7 years old and you could say I am a native here at this point. I am very passionate about working with others, the collaboration of many ideas fills my passion on being a Full Stack Web Developer. I am no stranger to the weird and uncomfortable, and I feel the best way to learn is to place yourself in the unfamiliar and unknown as your own results can and will surprise you!</p>
         </div>
-        <div class="card-action">
-          <a href="#">This is a link</a>
-          <a href="#">This is a link</a>
+
+        <div class="col s6">
+          
+        <img height="400" src="https://github.com/Jorgelopez5252/reactportfolio/blob/master/public/images/portphoto.jpeg?raw=true" alt="me"/>
+        </div>
+
+        <div class="col s12">
+          This div is 12-columns wide on all screen sizes and contact details
+          will go here
         </div>
       </div>
     </div>
-  </div>
   );
 }
 
