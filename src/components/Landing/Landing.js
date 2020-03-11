68da8e6e4ce369f237703e337c5d9d63c1275b3f
Jorge Lopez <JorgeLopez5252@gmail.com>
2020-03-10 19:19:52 -0700

    updated

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -4,11 +4,11 @@ import './Landing.css';
 
 function Landing() {
   return (
-    <div>
+    <div className="container"> 
       <Parallax
         paragraph="Full Stack Web Developer with a background in Banking/Financing and life-long dedication to learning. Effective at combining creativity and problem solving to develop user-friendly applications. Known among staff for strong wit and attention to detail no matter the complexity of the project."
         title="Jorge Antonio Lopez"
-        url="https://scontent.fphx1-2.fna.fbcdn.net/v/t1.0-9/556280_419147001430778_2117318718_n.jpg?_nc_cat=100&_nc_ohc=QKfiUVoZ4vEAX8EVoBO&_nc_ht=scontent.fphx1-2.fna&oh=bdc5935885393403e7cd9a7569bacdeb&oe=5EF2F496"
+        url="https://github.com/Jorgelopez5252/reactportfolio/blob/master/public/images/cardinalstadium.jpg?raw=true"
       />
 
       <Parallax
@@ -26,7 +26,7 @@ function Landing() {
       <Parallax
         paragraph="JSX, Rendering Elements, Great Understanding of Components and Props as well as State and hooks ."
         title="React.js"
-        url="https://github.com/Jorgelopez5252/reactportfolio/blob/master/public/images/reactWhite.png?raw=true"
+        url="https://github.com/Jorgelopez5252/reactportfolio/blob/master/public/images/React-logo.jpg?raw=true"
       />
       
     </div>
